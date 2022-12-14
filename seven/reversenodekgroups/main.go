2a3ee0a04fe4c38399a87458ab5ffae224655114
ozzy ozbourne <khanosaid726@gmail.com>
2022-12-14 22:24:42 +0530

    two more

---
+++ seven/reversenodekgroups/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	a := ListNode{
+		Val: 10,
+		Next: &ListNode{
+			Val: 20,
+			Next: &ListNode{
+				Val: 30,
+				Next: &ListNode{
+					Val:  40,
+					Next: nil,
+				},
+			},
+		},
+	}
+	printListNode(reverseKGroup(&a, 2))
+}
+
+func printListNode(q *ListNode) {
+	for q != nil {
+		fmt.Println(q.Val)
+		q = q.Next
+	}
+}
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	return nil
+}
