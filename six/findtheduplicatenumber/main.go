3e95190fd2f71d27c4019634ccf137c4595406f8
ozzy ozbourne <khanosaid726@gmail.com>
2022-12-28 01:52:28 +0530

    just one fix

--- six/findtheduplicatenumber/main.go
+++ six/findtheduplicatenumber/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(findDuplicate([]int{1, 3, 4, 2, 2}))
+}
+
+func findDuplicate(nums []int) int {
+	i := 0
+	for i < len(nums) {
+		if nums[i] != i+1 {
+			if nums[i] != nums[nums[i]-1] {
+				temp := nums[i]
+				nums[i] = nums[temp-1]
+				nums[temp-1] = temp
+			} else {
+				return nums[i]
+			}
+		} else {
+			i++
+		}
+	}
+	return -1
 }
