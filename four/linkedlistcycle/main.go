a6b11fd39df9f02251e199692203d31c1367f571
ozzy ozbourne <khanosaid726@gmail.com>
2022-12-30 03:24:56 +0530

    one more fix

--- four/linkedlistcycle/main.go
+++ four/linkedlistcycle/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import "fmt"
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	a0ne := ListNode{
+		Val:  1,
+		Next: nil,
+	}
+
+	aTwo := ListNode{
+		Val:  2,
+		Next: nil,
+	}
+
+	a0ne.Next = &aTwo
+	aTwo.Next = &a0ne
+
+	fmt.Println(hasCycle(&a0ne))
+}
+
+func hasCycle(head *ListNode) bool {
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
 }
