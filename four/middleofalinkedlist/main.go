79e25b2b6d14d442f8de6d1ab7f04309886a1043
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-02 04:03:08 +0530

    jan 1

--- four/middleofalinkedlist/main.go
+++ four/middleofalinkedlist/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import "fmt"
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	a := ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val:  5,
+						Next: nil,
+					},
+				},
+			},
+		},
+	}
+	printList(middleNode(&a))
+}
+
+func printList(a *ListNode) {
+	for a != nil {
+		fmt.Print(a.Val, "\t")
+		a = a.Next
+	}
+}
+
+func middleNode(head *ListNode) *ListNode {
+	f := head
+	s := head
+	for f != nil && f.Next != nil {
+		s = s.Next
+		f = f.Next.Next
+	}
+	return s
 }
