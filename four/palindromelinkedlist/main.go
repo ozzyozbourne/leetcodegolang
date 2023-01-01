ca6a6144715878ac938d3dad2fc30d8de8ba0b6c
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-02 04:16:56 +0530

    jan 1

--- four/palindromelinkedlist/main.go
+++ four/palindromelinkedlist/main.go
@@ -1,5 +1,54 @@
 package main
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 func main() {
 
+}
+
+func isPalindrome(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return true
+	}
+
+	f := head
+	s := head
+
+	for f != nil && f.Next != nil {
+		s = s.Next
+		f = f.Next.Next
+	}
+
+	rv := reverseList(s)
+	cRv := rv
+
+	for head != nil && rv != nil {
+
+		if head.Val != rv.Val {
+			break
+		}
+		head = head.Next
+		rv = rv.Next
+	}
+
+	reverseList(cRv)
+	if head == nil || rv == nil {
+		return true
+	}
+
+	return false
+}
+
+func reverseList(h *ListNode) *ListNode {
+	var pr *ListNode
+	for h != nil {
+		tp := h.Next
+		h.Next = pr
+		pr = h
+		h = tp
+	}
+	return pr
 }
