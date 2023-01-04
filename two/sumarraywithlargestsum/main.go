893160ad11f6ea75e0838149b45e222ce5713dca
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-05 05:22:17 +0530

    jan 4

--- two/sumarraywithlargestsum/main.go
+++ two/sumarraywithlargestsum/main.go
@@ -1,9 +1,20 @@
 package main
 
+import "math"
+
 func main() {
 
 }
 
 func maxSubArray(nums []int) int {
-
+	l := len(nums)
+	var lmax int
+	gmax := math.MinInt
+	for i := 0; i < l; i++ {
+		lmax = int(math.Max(float64(nums[i]), float64(nums[i]+lmax)))
+		if lmax > gmax {
+			gmax = lmax
+		}
+	}
+	return gmax
 }
