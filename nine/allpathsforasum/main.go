2a60651218f95d1de294fcd262c052ec9a9871a0
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-07 05:10:33 +0530

    jan 6

--- nine/allpathsforasum/main.go
+++ nine/allpathsforasum/main.go
@@ -1,5 +1,62 @@
 package main
 
-func main() {
+import "fmt"
 
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	//a := TreeNode{
+	//	Val: 1,
+	//	Left: &TreeNode{
+	//		Val:   2,
+	//		Left:  nil,
+	//		Right: nil,
+	//	},
+	//	Right: nil,
+	//}
+	//r := pathSum(&a, 3)
+	//fmt.Println(r)
+	a := make([]int, 0)
+	tester(a, 1)
+	fmt.Println(a)
+}
+
+func tester(a []int, k int) {
+	if k >= 3 {
+		return
+	} else {
+
+		a = append(a, k)
+	}
+	k++
+	tester(a, k)
+}
+
+func pathSum(root *TreeNode, targetSum int) [][]int {
+	allPaths := make([][]int, 0)
+	currentPath := make([]int, 0)
+	return findPathRec(root, targetSum, currentPath, allPaths)
+}
+
+func findPathRec(currentNode *TreeNode, sum int, currentPath []int, allPaths [][]int) [][]int {
+
+	if currentNode == nil {
+		return allPaths
+	}
+	currentPath = append(currentPath, currentNode.Val)
+
+	if currentNode.Val == sum && currentNode.Left == nil && currentNode.Right == nil {
+		copySlice := make([]int, len(currentPath))
+		copy(copySlice, currentPath)
+		allPaths = append(allPaths, copySlice)
+	} else {
+		allPaths = findPathRec(currentNode.Left, sum-currentNode.Val, currentPath, allPaths)
+		allPaths = findPathRec(currentNode.Right, sum-currentNode.Val, currentPath, allPaths)
+	}
+
+	return allPaths
 }
---
+++ two/longestsubstringwithkdistinctcharacters/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+
+}
+
+func longestSumStringlen(s string, k int) int {
+	if s == "" || len(s) < k {
+		return -1
+	}
+	var s, m int
+	mp := make()
+}
