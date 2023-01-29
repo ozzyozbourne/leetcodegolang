e894a51283b5854e27b2bc5c9532cf1fa5dd8e1a
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-29 19:34:30 +0530

    one more fix

--- two/findallanagramsinastring/main.go
+++ two/findallanagramsinastring/main.go
@@ -3,3 +3,33 @@ package main
 func main() {
 
 }
+
+func findAnagrams(s string, p string) []int {
+	var res []int
+	fmap := make(map[int32]int)
+	for _, v := range p {
+		fmap[v] += +1
+	}
+	var st, m int
+	for i, v := range s {
+		if _, ok := fmap[v]; ok {
+			fmap[v] -= 1
+			if fmap[v] == 0 {
+				m++
+			}
+		}
+		if m == len(fmap) {
+			res = append(res, st)
+		}
+		if i >= len(p)-1 {
+			if _, ok := fmap[int32(s[st])]; ok {
+				if fmap[int32(s[st])] == 0 {
+					m--
+				}
+				fmap[int32(s[st])] += 1
+			}
+			st++
+		}
+	}
+	return res
+}
