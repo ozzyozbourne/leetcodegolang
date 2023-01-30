6db74d94db2bdba612ace784191e4e61fe13b7f8
ozzy ozbourne <khanosaid726@gmail.com>
2023-01-30 23:21:55 +0530

    just one fix

--- three/tripletwithsmallersum/main.go
+++ three/tripletwithsmallersum/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sort"
+)
 
+func main() {
+	fmt.Println(searchTriplets([]int{-1, 0, 2, 3}, 3))
+	fmt.Println(searchTriplets([]int{-1, 4, 2, 1, 3}, 5))
+
+}
+
+func searchTriplets(arr []int, target int) int {
+	sort.Ints(arr)
+	var count int
+	for i := 0; i < len(arr)-2; i++ {
+		count += searchPair(arr, target-arr[i], i+1)
+	}
+	return count
+}
+
+func searchPair(arr []int, target int, left int) int {
+	var count int
+	right := len(arr) - 1
+	for left < right {
+		if arr[left]+arr[right] < target {
+			count += right - left
+			left++
+		} else {
+			right--
+		}
+	}
+	return count
 }
